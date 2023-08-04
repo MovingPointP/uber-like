55826317dc49711ba95736230e81ec0968956b26
movingpointp <kobayashi.akio39@gmail.com>
2023-08-04 16:18:50 +0900

    [ADD]restrantページ

---
+++ frontend/src/apis/restaurants.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { restaurantsIndex } from '../urls/index'
+
+export const fetchRestaurants = async () => {
+  try {
+    const result = await axios.get(restaurantsIndex)
+    return result.data as restaurants
+  } catch (error){
+    console.error(error)
+    return null
+  }
+}
+
+type restaurants  = {
+  restaurans: {
+    id: number,
+    name: string,
+    fee: number,
+    time_required: number,
+    created_at: string,
+    updated_at: string
+  }[]
+}
--- frontend/src/containers/Restaurants.tsx
+++ frontend/src/containers/Restaurants.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Fragment } from "react"
+
+import { fetchRestaurants } from '../apis/restaurants';
+
+useEffect(() => {
+  (async() => { 
+  const result = await fetchRestaurants()
+  console.log(result)
+  })()
+},[])
 
 export const Restaurants = () => {
   return (
